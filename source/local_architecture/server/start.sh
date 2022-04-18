9ae332e16da25470391afc3717ad0b503d8d311d
simone-viozzi <viozzis333@gmail.com>
2022-04-18 20:54:55 +0200

    Fixing Lyntor's bugs.

--- source/local_architecture/server/start.sh
+++ source/local_architecture/server/start.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 sudo iptables -I INPUT -p udp -m udp --dport 1194 -j ACCEPT
-sudo iptables -t nat -A POSTROUTING -s 10.0.4.0/24 -o eth0@if151 -j MASQUERADE
+sudo iptables -t nat -A POSTROUTING -s 10.0.4.0/24 -o eth0 -j MASQUERADE
 sudo iptables --policy FORWARD ACCEPT
 
 cd /etc/openvpn/server
