4e6036bbe2c5c858fde6a4d7036e4e02b39b304b
simone-viozzi <viozzis333@gmail.com>
2022-04-20 14:21:36 +0200

    fix

--- source/local_architecture/server/start.sh
+++ source/local_architecture/server/start.sh
@@ -1,8 +1,13 @@
 #! /bin/sh
 
+# <-- Configurazione del firewall -->
 sudo iptables -I INPUT -p udp -m udp --dport 1194 -j ACCEPT
+
 sudo iptables -t nat -A POSTROUTING -s 10.0.4.0/24 -o eth0 -j MASQUERADE
+
 sudo iptables --policy FORWARD ACCEPT
+
+
 
 cd /etc/openvpn/server
 sudo openvpn --status ./openvpn-server/status.log --status-version 2 --suppress-timestamps --config /etc/openvpn/server/server.conf
