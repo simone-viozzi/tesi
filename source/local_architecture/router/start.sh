ddc2e9ebede0550a20290803ad0997c5fdb4aa96
simone-viozzi <viozzis333@gmail.com>
2022-04-30 20:22:20 +0200

    I transformed a bug into a feature. Once you learn how, you'll never forget it

--- source/local_architecture/router/start.sh
+++ source/local_architecture/router/start.sh
@@ -1,11 +1,12 @@
 #! /bin/sh
 
 init(){
-sleep 10
-    openvpn --config /configs/router1.ovpn &
-    sleep 3
+    #sleep 10
+    #openvpn --config /configs/router1.ovpn &
+    
+    sleep 10
     ping -c 2 10.8.0.1
 }
 
-(init) &
+#(init) &
 exec /sbin/init
