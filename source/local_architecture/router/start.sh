4d0242695439c0cd56cf64abaacba869c6befae7
simone-viozzi <viozzis333@gmail.com>
2022-05-01 12:17:36 +0200

    Improvements

--- source/local_architecture/router/start.sh
+++ source/local_architecture/router/start.sh
@@ -1,12 +1,17 @@
 #! /bin/sh
 
 init(){
+    sleep 5
+    sed -i '10s|.*|        option enabled 1|' /etc/config/openvpn
+    sed -i '20s|.*|        option config /configs/router1.ovpn|' /etc/config/openvpn
+
+    service openvpn restart
     #sleep 10
     #openvpn --config /configs/router1.ovpn &
     
-    sleep 10
+    sleep 2
     ping -c 2 10.8.0.1
 }
 
-#(init) &
+(init) &
 exec /sbin/init
