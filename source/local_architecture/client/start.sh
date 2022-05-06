9de8fbdd010f5381d7f323960b89ba2d2e315e50
simone-viozzi <viozzis333@gmail.com>
2022-05-06 23:04:27 +0200

    extra debug for stuff module

--- source/local_architecture/client/start.sh
+++ source/local_architecture/client/start.sh
@@ -4,7 +4,7 @@ sleep 1.5
 
 sudo openvpn --config /home/ubuntu/configs/client1.ovpn &
 
-sleep 3
+sleep 5
 
 ping -c 2 10.8.0.1 >/dev/null || echo "-----------------------\nserver is down\n-----------------------"
 ping -c 2 10.8.0.254 >/dev/null || echo "-----------------------\nclient to client is off\n-----------------------"
