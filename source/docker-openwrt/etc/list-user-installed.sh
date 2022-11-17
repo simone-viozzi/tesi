ee0cff35d7ed28851a0be531f45e93b106689e65
simone-viozzi <viozzis333@gmail.com>
2022-11-17 15:35:13 +0100

    <html> <head><title>301 Moved Permanently</title></head> <body> <center><h1>301 Moved Permanently</h1></center> </body> </html>

--- source/docker-openwrt/etc/list-user-installed.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# source: https://openwrt.org/docs/guide-user/installation/generic.sysupgrade
-
-awk '/^Package:/{PKG= $2}
-/^Status: .*user installed/{print PKG}' /usr/lib/opkg/status | sort
